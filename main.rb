10ad14f158b153299c162a34bf2ff62e01623268
鯛 <pf529crm@s.okayama-u.ac.jp>
2025-09-11 13:05:56 +0900

    難易度設定を撤廃。モードでの分類へ

--- main.rb
+++ main.rb
@@ -10,34 +10,42 @@ class TypingGame < Gosu::Window
         @big_font = Gosu::Font.new(48)
         @input = ""
 
+        # 得点管理
         @score = 0
         @combo = 0
         @max_combo = 0
 
+        # 状態管理
         @state = :title
 
+        # 言語とモード選択
+        @selected_language = nil
+        @language_index = 0
         @selected_mode = nil
         @mode_index = 0
-        @selected_level = nil
-        @level_index = 0
 
+        # 問題
         @words_data = load_words("question.json")
         @current_word = ""
 
+        # 効果音
         @type_sound = Gosu::Sample.new("sound/type.wav")
         @miss_sound = Gosu::Sample.new("sound/miss.wav")
         @back_sound = Gosu::Song.new("sound/back.wav")
 
+        # 時間の管理
         @time_limit = 30
         @remaining_time = @time_limit
         @last_tick = 0
     end
 
+    # 問題ファイルの読み込み
     def load_words(path)
         JSON.parse(File.read(path))
     end
 
     def update
+        # 状態に応じて処理を分岐
         case @state
             when :title
                 return
@@ -63,13 +71,14 @@ class TypingGame < Gosu::Window
     end
 
     def draw
+        # 状態に適した描画関数を呼び出し
         case @state
             when :title
                 draw_title_screen
+            when :language_select
+                draw_language_select_screen
             when :mode_select
                 draw_mode_select_screen
-            when :level_select
-                draw_level_select_screen
             when :game
                 draw_game_screen
             when :result
@@ -77,28 +86,35 @@ class TypingGame < Gosu::Window
         end
     end
 
+    # タイトル画面の描画　
     def draw_title_screen
         @big_font.draw_text("Typing Game", 220, 200, 0, 1.5, 1.5, Gosu::Color::BLUE)
         @font.draw_text("Press Enter to Start", 250, 300, 0, 1.0, 1.0, Gosu::Color::WHITE)
     end
 
+    # 言語選択画面の描画
+    def draw_language_select_screen
+        @big_font.draw_text("Select Language", 210, 150, 0, 1.2, 1.2, Gosu::Color::AQUA)
+        @words_data.keys.each_with_index do |language, i|
+            # 選択中の言語のみハイライト
+            color = (i == @language_index ? Gosu::Color::YELLOW : Gosu::Color::WHITE)
+            @font.draw_text("#{i + 1}. #{language}", 300, 220 + i * 50, 0, 1.2, 1.2, color)
+        end
+    end
+
+    # モード選択画面の描画
     def draw_mode_select_screen
-        @big_font.draw_text("Select Mode", 280, 150, 0, 1.2, 1.2, Gosu::Color::AQUA)
-        @words_data.keys.each_with_index do |mode, i|
+        modes = @words_data[@selected_language].keys
+        @big_font.draw_text("Select Mode (#{@selected_language})", 160, 150, 0, 1.2, 1.2, Gosu::Color::AQUA)
+        modes.each_with_index do |mode, i|
+            # 選択中のモードのみハイライト
             color = (i == @mode_index ? Gosu::Color::YELLOW : Gosu::Color::WHITE)
             @font.draw_text("#{i + 1}. #{mode}", 300, 220 + i * 50, 0, 1.2, 1.2, color)
         end
+        @font.draw_text("Press ← Key to return SELECT LANGUAGE", 20, 560, 0, 1.0, 1.0, Gosu::Color::GRAY)
     end
 
-    def draw_level_select_screen
-        levels = ["easy", "normal", "hard"]
-        levels.each_with_index do |level, i|
-            color = (i == @level_index ? Gosu::Color::YELLOW : Gosu::Color::WHITE)
-            @font.draw_text("#{i + 1}. #{level.capitalize}", 300, 240 + i * 50, 0, 1.2, 1.2, color)
-        end
-        @font.draw_text("Press ← Key to return SELECT MODE", 20, 560, 0, 1.0, 1.0, Gosu::Color::GRAY)
-    end
-
+    # プレイ画面の描画
     def draw_game_screen
         @font.draw_text("Time: #{@remaining_time}", 650, 30, 0, 1.0, 1.0, Gosu::Color::RED)
 
@@ -112,6 +128,7 @@ class TypingGame < Gosu::Window
         @font.draw_text("Combo: #{@combo}", 50, 540, 0)
     end
 
+    # リザルト画面の描画
     def draw_result_screen
         @big_font.draw_text("Time's up!", 280, 180, 0, 1.3, 1.3, Gosu::Color::FUCHSIA)
         @font.draw_text("Your score: #{@score}", 320, 270, 0)
@@ -119,49 +136,51 @@ class TypingGame < Gosu::Window
         @font.draw_text("Press Enter to return to Title", 240, 350, 0)
     end
     
+    # キー入力の処理
     def button_down(id)
         case @state
             when :title
+                # 言語選択画面へ移行もしくは終了
                 if id == Gosu::KB_RETURN
-                    @state = :mode_select
+                    @state = :language_select
                 elsif id == Gosu::KB_ESCAPE
                     close
                 end
 
-            when :mode_select
-                modes = @words_data.keys
+            when :language_select
+                languages = @words_data.keys
                 
+                case id
+                when Gosu::KB_DOWN
+                    @language_index = (@language_index + 1) % languages.size
+                when Gosu::KB_UP
+                    @language_index = (@language_index - 1) % languages.size
+                when Gosu::KB_RETURN
+                    @selected_language = languages[@language_index]
+                    @state = :mode_select
+                when Gosu::KB_1 .. Gosu::KB_9
+                    index = id - Gosu::KB_1
+                    if index < languages.size
+                        @selected_language = languages[index]
+                        @state = :mode_select
+                    end
+                end
+        
+            when :mode_select
+                modes = @words_data[@selected_language].keys
+
                 case id
                 when Gosu::KB_DOWN
                     @mode_index = (@mode_index + 1) % modes.size
                 when Gosu::KB_UP
                     @mode_index = (@mode_index - 1) % modes.size
                 when Gosu::KB_RETURN
-                    @selected_mode = modes[@mode_index]
-                    @state = :level_select
-                when Gosu::KB_1 .. Gosu::KB_9
-                    index = id - Gosu::KB_1
-                    if index < modes.size
-                        @selected_mode = modes[index]
-                        @state = :level_select
-                    end
-                end
-        
-            when :level_select
-                levels = ["easy", "normal", "hard"]
-
-                case id
-                when Gosu::KB_DOWN
-                    @level_index = (@level_index + 1) % levels.size
-                when Gosu::KB_UP
-                    @level_index = (@level_index - 1) % levels.size
-                when Gosu::KB_RETURN
-                    start_game(levels[@level_index])
+                    start_game(modes[@mode_index])
                 when Gosu::KB_LEFT
-                    @state = :mode_select
+                    @state = :language_select
                 when Gosu::KB_1 .. Gosu::KB_3
                     index = id - Gosu::KB_1
-                    start_game(levels[index]) if index < levels.size
+                    start_game(modes[index]) if index < modes.size
                 end
 
             when :game
@@ -191,9 +210,10 @@ class TypingGame < Gosu::Window
         end
     end
 
-    def start_game(level)
-        @selected_level = level 
-        @words = @words_data[@selected_mode][@selected_level]
+    # ゲーム開始時の初期化を行う
+    def start_game(mode)
+        @selected_mode = mode 
+        @words = @words_data[@selected_language][@selected_mode]
         @used_words = []
 
         @state = :game
